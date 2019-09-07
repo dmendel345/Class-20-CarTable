f52092e3a1ff40a1576c721a503d4dbb73470d91
dmendel345 <dmendel345@gmail.com>
2019-09-07 18:15:35 +0300

    Form added

---
+++ build-table.js
@@ -0,0 +1,9 @@
+
+//************* */
+    // Generate table
+    function generateTableHead(table) {
+        let thead = table.createTHead();
+      }
+
+      let table = document.querySelector("table");
+generateTableHead(table);
